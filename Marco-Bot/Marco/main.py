8dcd74d561ca8ea005a2b15db7e9003c40fe6476
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-07-17 18:41:26 +0300

    😄 Streamlit UI update

--- Marco-Bot/Marco/main.py
+++ Marco-Bot/Marco/main.py
@@ -34,9 +34,6 @@ def intro():
         Marco is a comprehensive event management platform designed primarily for travelers. It serves as a one-stop database for discovering, booking, and even hosting events. Whether you're a tourist exploring a new city or a local looking to uncover hidden gems, Marco has you covered. Our platform offers a list of diverse events and provides reservation and booking services. Additionally, event organizers can take advantage of Marco's 
         promotional services to boost their event's visibility, regardless of its location.
 
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-
         **👈 Select a demo from the dropdown on the left** to see some examples
         of what Marco can do!
 
